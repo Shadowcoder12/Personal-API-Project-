726940ee38b9ea8e7a2c87b8a208fb928331805d
lfonzi62 <lfontenette@berkeley.edu>
2019-02-14 13:57:54 -0800

    added wish model and schema

--- express-personal-api-master/seed.js
+++ express-personal-api-master/seed.js
@@ -1,7 +1,25 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// const db = require('./models');
+const db = require('./models');
+
+const LouieWish = {
+    item: "Lou Spike men Shoes",
+    description:"Designer Shoes",
+    price: 1200,
+    websiteLink: "http://us.christianlouboutin.com/us_en/shop/men/lou-spikes-men-s-flat.html"
+}
+
+
+db.Wish.create(LouieWish, (err, louieshoes) => {
+if (err) return err;
+console.log(`Created a new wish ${louieshoes} `);
+process.exit();
+})
+
+
+
+
 
 // const new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
