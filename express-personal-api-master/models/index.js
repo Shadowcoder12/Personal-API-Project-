cbd22683f6a22f3dd5d94a53b33c586c12dd587b
lfonzi62 <lfontenette@berkeley.edu>
2019-02-14 14:02:43 -0800

    exported wishes

--- express-personal-api-master/models/index.js
+++ express-personal-api-master/models/index.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
 
+const Wish = require("./wishes");
+
 // module.exports.Campsite = require("./campsite.js.example");
+
+exports.Wish = Wish;
