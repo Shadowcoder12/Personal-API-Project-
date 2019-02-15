50601c73fd798dc451e5038b3d04cf84973379b6
lfonzi62 <lfontenette@berkeley.edu>
2019-02-15 00:34:47 -0800

    created ajax call and success functions

--- express-personal-api-master/public/scripts/app.js
+++ express-personal-api-master/public/scripts/app.js
@@ -1,7 +1,27 @@
 console.log("Sanity Check: JS is working!");
 
+
+const handleResponse = (json) => {
+    let wishData = json;
+    for (var i =0 ;  i < wishData.length; i++) {
+    let item = wishData[i].item;
+    let price = wishData[i].price;
+    let URL = wishData[i].websiteLink;
+    let description = wishData[i].description;
+    $('.wishItems').append(`<div class="text-center"> <strong><p class="item"> ${item}: </strong> ${description} -  <span class="price"> $${price} </span> </p> <div>`)
+    };
+    }
+
+
+   const handleError = (xhr, status, errorThrown) => console.log('uh oh');
+
 $(document).ready(function(){
 
-// your code
+    $.ajax({
+    method: 'GET',
+    url: '/api/wish',
+    success: handleResponse,
+    error: handleError
+});
 
 });
