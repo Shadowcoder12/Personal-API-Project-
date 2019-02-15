b5fbcde83d92a284e45fbc3b7b29f26bcd589906
lfonzi62 <lfontenette@berkeley.edu>
2019-02-14 16:05:17 -0800

    cleaned up seed data format

--- express-personal-api-master/seed.js
+++ express-personal-api-master/seed.js
@@ -1,7 +1,13 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
+// HEROKU BASH SEEDING
+// make changes to local seed files first, then push file changes to heroku "push origing heroku master"
+// then run following command.  1. heroku run bash 2. node seed.js
 const db = require('./models');
+
+
+// Array of different wish items
 
 const wishList = [
     {
@@ -18,44 +24,36 @@ const wishList = [
     },
 ];
 
+// SEEDING THE DATA IN THE DB
 
-
-// const LouieWish = {
-//     item: "Lou Spike men Shoes",
-//     description:"Designer Shoes",
-//     price: 1200,
-//     websiteLink: "http://us.christianlouboutin.com/us_en/shop/men/lou-spikes-men-s-flat.html"
-// }
-
-// CREATING THE LOUIE SHOES IN THE DATABASE
-
+db.Wish.deleteMany({}, function(err, Wish) {
+console.log(`Deleted ${Wish.length} ${Wish}`);
 db.Wish.create(wishList, (err, wishes) => {
     if (err) return err;
     console.log(`Created ${wishes.length} wishes`);
     process.exit();
 });
+})
 
-// const monclierWish = {
-//     item:"Monclier maya jacket",
-//     description:"overpriced puffer coat",
-//     price: 1400,
-//     websiteLink:"https://store.moncler.com/en-us/outerwear_cod10045112022146586.html?gclid=EAIaIQobChMIosGyvaa84AIVciCtBh3SzQ01EAQYAiABEgLZUPD_BwE&gclsrc=aw.ds&tp=70950"
-// }
+// db.Wish.find({}, (err, wishList) => {
+//     if (err) {
+//     console.log(err);
+//     }
+//     console.log(wishList);
+//     });
 
-// db.Wish.create(monclierWish, (err, monclerJacket) => {
-// if (err) return err;
-// console.log(`Created a new wish ${monclerJacket} `);
-// process.exit();
-// })
-
-db.Wish.find({}, (err, wishList) => {
-    if (err) {
-    console.log(err);
-    }
-    console.log(wishList);
-    });
+// ORIGINAL CREATE FUNCTION
+//  db.Wish.create(wishList, (err, wishes) => {
+//     if (err) return err;
+//     console.log(`Created ${wishes.length} wishes`);
+//     process.exit();
+// });
 
 
+
+
+    
+// EXAMPLE
 
 // const new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
