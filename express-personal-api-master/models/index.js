ad12c49da2fc364a001f6502aa12b2b6bf69f7af
lfonzi62 <lfontenette@berkeley.edu>
2019-02-14 17:43:43 -0800

    updating Heroku code

--- express-personal-api-master/models/index.js
+++ express-personal-api-master/models/index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", { useNewUrlParser: true });
 
 const Wish = require("./wishes");
-
 // module.exports.Campsite = require("./campsite.js.example");
 
 exports.Wish = Wish;
